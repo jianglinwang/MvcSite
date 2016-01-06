44fc59e8254f894a608c4e4c97821ff02b277a22
jianglin <jianglin.wang@outlook.com>
2016-01-06 23:58:09 +0800

    implemetn

---
+++ MvcSite/MvcSite.Core/SqlRepository/SqlRepositoryExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcSite.Core.SqlRepository
+{
+    public static class SqlRepositoryExtension
+    {
+        public static async Task ExecuteStoredProcedure(this ISqlRepository repository, string procedureName, DataTable dt)
+        {
+            using (var conn = repository.Create())
+            {
+                var sqlCommand = conn.CreateCommand();
+                sqlCommand.CommandText = procedureName;
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                try
+                {
+                    await conn.OpenAsync();
+
+                    await sqlCommand.ExecuteNonQueryAsync();
+
+                    var adapter = new SqlDataAdapter(sqlCommand);
+                    adapter.Fill(dt);
+                }
+                finally { conn.Close(); }
+            }
+        }
+    }
+}
